47e2dca42e2a864b0f9de61309f4bca3df6d2e7f
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-08-19 20:11:40 +0200

    :rocket: first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use std::{process, str::FromStr};
+use tracing_subscriber::{
+    EnvFilter,
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
+
+use models::config::Configuration;
+
+mod http;
+mod models;
+
+#[tokio::main]
+async fn main(){
+    let configuration = read_configuration().await;
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_str(configuration.get_log_level()).unwrap())
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    http::serve(configuration).await.unwrap();
+}
+
+async fn read_configuration() -> Configuration{
+    let content = match tokio::fs::read_to_string("config.yml")
+        .await {
+            Ok(value) => value,
+            Err(e) => {
+                println!("Error with config file `config.yml`: {}",
+                    e.to_string());
+                process::exit(0);
+            }
+        };
+    match Configuration::new(&content){
+        Ok(configuration) => configuration,
+        Err(e) => {
+            println!("Error with config file `config.yml`: {}",
+                e.to_string());
+            process::exit(0);
+        }
+    }
 }
