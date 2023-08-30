80e6fb8d772237faac13d8e9b60e857aa95ff7c5
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-08-30 07:12:58 +0200

    :sparkles: read app from labels

--- src/models/app.rs
+++ src/models/app.rs
@@ -1,12 +1,51 @@
+use docker_api::models::ContainerSummary;
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct App{
     pub name: String,
-    pub url: String,
-    pub icon: String,
+    pub url: Option<String>,
+    pub icon: Option<String>,
     pub new_tab: bool,
-    pub container_name: String,
-    pub image_name: String,
-    pub state: bool,
+    pub container_id: Option<String>,
+    pub container_name: Option<Vec<String>>,
+    pub image_name: Option<String>,
+    pub image_id: Option<String>,
+    pub state: Option<String>,
+}
+
+impl App{
+    pub fn from_summary(summary: ContainerSummary) -> Self{
+        let labels = summary.labels.unwrap();
+
+        let name =  labels.get("es.atareao.board.name").unwrap().to_string();
+        let url = match &labels.get("es.atareao.board.url"){
+            Some(url) => Some(url.to_string()),
+            None => None
+        };
+        let icon = match &labels.get("es.atareao.board.icon"){
+            Some(icon) => Some(icon.to_string()),
+            None => None
+        };
+        let new_tab = match &labels.get("es.atareao.board.new_tab"){
+            Some(new_tab) => *new_tab == "true",
+            None => true,
+        };
+        let container_id = summary.id;
+        let container_name = summary.names;
+        let image_name = summary.image;
+        let image_id = summary.image_id;
+        let state = summary.state;
+        Self{
+            name,
+            url,
+            icon,
+            new_tab,
+            container_id,
+            container_name,
+            image_name,
+            image_id,
+            state
+        }
+    }
 }
