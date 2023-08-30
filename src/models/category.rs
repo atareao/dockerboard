1db17ebe2c6711d9281e70c75940d6a4cfdfd7d6
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-08-30 07:13:17 +0200

    :art: categories

--- src/models/category.rs
+++ src/models/category.rs
@@ -1,5 +1,4 @@
 use serde::{Serialize, Deserialize};
-use super::app::App;
 use docker_api::{
     Docker,
     opts::{
@@ -7,16 +6,25 @@ use docker_api::{
         ContainerFilter
     }, models::ContainerSummary,
 };
+use tracing::{info, error};
+use super::{error::DBError, App};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Category{
     pub name: String,
     pub icon: String,
     pub description: String,
+    #[serde(default = "get_default_apps")]
+    pub apps: Vec<App>,
+}
+
+fn get_default_apps() -> Vec<App>{
+    Vec::new()
 }
 
 impl Category {
-    pub async fn get_containers(&self, docker: &Docker) -> Result<Vec<ContainerSummary>, docker_api::Error>{
+    pub async fn get_containers(&self, docker: &Docker) -> Vec<App>{
+        info!("get_containers");
         let mut filters: Vec<ContainerFilter> = Vec::new();
         filters.push(ContainerFilter::Label(
             "es.atareao.board.active".to_string(),
@@ -29,6 +37,18 @@ impl Category {
         let opts = ContainerListOpts::builder()
             .filter(filters)
             .build();
-        docker.containers().list(&opts).await
+        match docker.containers().list(&opts).await{
+            Err(e) => {
+                error!("{:?}", e);
+                Vec::new()
+            },
+            Ok(container_summaries) => {
+                let mut containers: Vec<App> = Vec::new();
+                for summary in container_summaries.into_iter(){
+                    containers.push(App::from_summary(summary));
+                }
+                containers
+            }
+        }
     }
 }
